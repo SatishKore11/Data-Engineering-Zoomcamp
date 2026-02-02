b2be4b425c58f1c0c82171e7eed5b82ba502c090
Satish  Sampatrao Kore <154883166+SatishKore11@users.noreply.github.com>
2026-02-02 17:38:09 +0000

    workflow-orchestration

--- TERRADEMO/main.tf
+++ TERRADEMO/main.tf
@@ -8,14 +8,15 @@ terraform {
 }
 
 provider "google" {
-  project = "terraform-new-485514"
-  region  = "us-central1"
+  credentials = var.credentials
+  project     = var.project
+  region      = var.region
 }
 
 
 resource "google_storage_bucket" "demo-bucket" {
-  name          = "terraform-new-485514-terra-bucket"
-  location      = "US"
+  name          = var.gcs_bucket_name
+  location      = var.location
   force_destroy = true
 
   lifecycle_rule {
@@ -26,4 +27,10 @@ resource "google_storage_bucket" "demo-bucket" {
       type = "AbortIncompleteMultipartUpload"
     }
   }
+}
+
+
+resource "google_bigquery_dataset" "demo_dataset" {
+  dataset_id = var.bq_dataset_name
+  location   = var.location
 }
